95269dae65d07ec36ec227179485ffc8dd0aa905
emmikb <emilykayburchfield@gmail.com>
2016-06-07 15:02:09 -0500

    working models except for k issue in ps

--- Code/gw_kriging.R
+++ Code/gw_kriging.R
@@ -15,13 +15,10 @@ library(rgdal)
 #any preprocessing of groundwater elevtaion data?  detrend?  log
 
 #random sample of full dataset
-#build sample vg
-#id best modeled vg
-
 #dr <- '/data/emily/WF/kate/gw/'
 #d <- read.csv(paste(dr,'gw.csv',sep=''), stringsAsFactors=FALSE)
 
-dr <- 'C:\\Users\\Emily\\Dropbox\\Vanderbilt\\Kate_Emily\\Code\\'
+dr <- 'C:\\Users\\Emily\\Dropbox\\Vanderbilt\\Kate_Emily\\CA_drought\\Code\\'
 d <- as.data.frame(read.table(paste(dr, 'dsub.txt', sep=''), header=T), stringsAsFactors=F)
 coordinates(d) <- ~LON+LAT
 
@@ -33,166 +30,157 @@ sto <- stConstruct(df, c("LAT", "LON"), "TIME", interval = FALSE)  #time instanc
 sto@sp@proj4string <- CRS('+init=epsg:4326')  #WGS84
 stplot(sto)
 
-#acf
-acf(sto@data, 'xts')  #up to 200 days significant
-
 #space-time SAMPLE variogram
 sampleVar <- variogramST(LOG_ELEV~1, sto, tunit="days", tlags=seq(from=0,to=225,by=30), cutoff = 100, na.omit=T)  #log elevation doesn't work because of negative values
-plot(sampleVar, map=F)
-plot(sampleVar, wireframe=T)
+
+#diagnostic plots
+acf(sto@data, 'xts')  #up to 200 days significant
+plot(sampleVar, map=F) #range of around 60, nugget less than 1
+plot(sampleVar, wireframe=T) #visualize space/time lag
 #for unprojected great circle distances (km).
 #gamma values conditional on treatment of data (log, etc)
 
-#parameters
-pars.l <- c(sill.s = 0, range.s = 10, nugget.s = 0,sill.t = 0, range.t = 1, 
-            nugget.t = 0,sill.st = 0, range.st = 10, nugget.st = 0, anis = 0)
-pars.u <- c(sill.s = 100, range.s = 1000, nugget.s = 100,sill.t = 200, range.t = 60, 
-            nugget.t = 100,sill.st = 200, range.st = 1000, nugget.st = 100,anis = 700) 
-modelVar <- vgmST("sumMetric", 
-                      space=vgm(20, "Exp", 150, 0.1),   #sill, model, range, nugget
-                      time=vgm(20, "Exp", 100, 0.1), 
-                      joint=vgm(20,"Exp",150,0),
-                      stAni=15)
+#estimate space-time anisotropy, or directional dependence of data
+#http://finzi.psych.upenn.edu/library/gstat/html/estiStAni.html
+estiStAni(sampleVar, c(10,150), "vgm", vgm(20,"Exp",120,0))
+#data too sparse?  http://stats.stackexchange.com/questions/35316/problems-estimating-anisotropy-parameters-for-a-spatial-model
 
-#check parameters
-extractPar(modelVar)
-plot(sampleVar, modelVar, map=F)
+#parameter range
+pars.l <- c(sill.s = 0, range.s = 10, nugget.s = 0, sill.t = 0, range.t = 10, 
+            nugget.t = 0, sill.st = 0, range.st = 10, nugget.st = 0, anis = 0)
+pars.u <- c(sill.s = 100, range.s = 100, nugget.s = 10,sill.t = 300, range.t = 100, 
+            nugget.t = 100, sill.st = 200, range.st = 1000, nugget.st = 100,anis = 700) 
 
-#test fit
-comp <- fit.StVariogram(sampleVar, modelVar, fit.method=0)
-attr(comp, "MSE")
-
-
-
-
-fit.StVariogram(sampleVar, vgm(c("Exp", "sph")))
-
-#variogram MODELS
-
-
-
-#metric model with st anisotropy
-metricVarJoint <- vgmST("metric", 
-                        joint=vgm(psill=10,"Mat",range=100, nugget=1), stAni=50)
-
-metricVarSep <- vgmST("separable", 
-                      space=vgm(-60, "Sph", 500, 1), 
-                      time=vgm(35, "Sph", 500, 1), sill = 100)
-
-#vgm(psill, model, range, nugget, add.to, annis, kappa)
-
-metricVar <- fit.StVariogram(sampleVar, metricVarSep)
-#RMS-difference between surfaces of metric and sampleVar
-attr(metricVar,"optim")$value 
-
-
-var <- variogramST(ELEV~LAT+LON, data=sto, tunit="days", tlags=seq(from=0, to=225, by=15), assumeRegular=F, na.omit=T)
-plot(var, map=F)
-#assuming separability, simple model
-
-#metric vgm
-metricVgm <- vgmST("metric", joint=vgm(50,"Exp", 100,0), stAni=50)
-metricVgm <- fit.StVariogram(var, metricVgm)
-plot(var, metricVgm)
-#
-
-
-#lower and upper limits for variogram parameters, now from example directly
-pars.l <- c(sill.s = 0, range.s = 10, nugget.s = 0,sill.t = 0, range.t = 1, 
-            nugget.t = 0,sill.st = 0, range.st = 10, nugget.st = 0, anis = 0)
-pars.u <- c(sill.s = 200, range.s = 1000, nugget.s = 100,sill.t = 200, range.t = 60, 
-            nugget.t = 100,sill.st = 200, range.st = 1000, nugget.st = 100,anis = 700)
-
-
-#for separable variogram model, create model for spatial AND temporal components
-sep <- vgmST("separable", space=vgm(-60, "Sph", 500, 1), time=vgm(35, "Sph", 500, 1), sill = 100)
-par(mfrow=c(1,2))
-
-
-#variogram plot
-par(mfrow=c(1,2))
-plot(var, map=F)
-plot(var, sep, map=F)
-
-
-
-
-
-
-
-
-
-
-
-
-
-#long format: each record reflects single space and time combination
-
-#create space-time object, http://www.inside-r.org/packages/cran/spacetime/docs/stConstruct
-d@data$LON <- d@coords[,1]
-d@data$LAT <- d@coords[,2]
-d@data$TIME <- as.Date(d@data$DATE)
-sto <- stConstruct(d@data, c("LAT", "LON"), "TIME", interval = FALSE)  #time instance, not time interval
-
-#variogram, http://www.inside-r.org/node/209774
-var <- variogramST(ELEV~1, locations=data@coords, data=sto, tunit="days", tlags=seq(from=15, to=90, by=15), assumeRegular=F, na.omit=T, progress=T)
-
-plot(var, map=F) #gama distance
-plot(var, map=T) #time distance
-plot(var, wireframe=T) #3d gamma distance time
-
+##################
+#metric model
+##################
 #http://geostat-course.org/system/files/part01.pdf
-tmp_pred <- data.frame(cbind(d@coords, d@data$TIME))
-colnames(tmp_pred) <- c("x", "y", "t")
-coordinates(tmp_pred) <- ~x+y+t
-blockKrige <- krige(ELEV~1, sto, newdata=tmp_pred, model=model3d) #add block?
+#Temporal domain rescaled to match spatial one.  Lag classes include space, time, and space-time distances.  
+#Using a single model forces all distances to behave in the same way (unlikely)
+#assumes identical covariance unctions for space and time
+#includes space-time anisitropy that allows some flexibility
+#all distances treated equally, only one joint variogram for all three.
+
+metVar <- vgmST("metric",
+                joint = vgm(20,"Exp",100,0), #sill, model, range, nugget
+                stAni = 100) #directional dependence
+metVarDF <- fit.StVariogram(sampleVar, metVar, method="L-BFGS-B", fit.method=0)
+MSEmetVarDF <- attr(metVarDF, "MSE")
+metVarBF <- fit.StVariogram(sampleVar, metVar, method="L-BFGS-B", fit.method=7, lower=pars.l, upper=pars.u)
+MSEmetVarBF <- attr(metVarBF, "MSE") 
+
+#fit.model = 7 is method that controls the weighing of the residuals between empirical and model surface;
+#7 is the ST analog to commonly used spatial weighting; higher confidence in distances with many pairs
+
+###################
+#product-sum
+##################
+#relax assummptions in covariance matrix, does not assume separability
+#need to provude both spatial and temporal components, plus k, weighting product
+
+psVar <- vgmST("productSum", 
+                space=vgm(15, "Exp", 60, 0),   #sill, model, range, nugget
+                time=vgm(15, "Exp", 40, 0),
+                k=100)
+psVarDF <- fit.StVariogram(sampleVar, psVar, method="L-BFGS-B", fit.method=0)  #data fit
+MSEpsVarDF <- attr(psVarDF, "MSE")
+psVarBF <- fit.StVariogram(sampleVar, psVar, method="L-BFGS-B", fit.method=7, lower=pars.l, upper=pars.u)
+#k must be positive error
+MSEpsVarBF <- attr(psVarBF, "MSE")
+
+####################
+#sum-metric
+####################
+#metric with spatial and temporal covariance models plus joint component with anisitropy
+#maximum flexibility
+
+smVar <- vgmST("sumMetric", 
+                space=vgm(15, "Exp", 60, 0),   #sill, model, range, nugget
+                time=vgm(15, "Exp", 40, 0),
+                joint=vgm(15,"Exp",80,0),
+                stAni=200)
+smVarDF <- fit.StVariogram(sampleVar, smVar, method="L-BFGS-B", fit.method=0)  
+MSEsmVarDF <- attr(smVarDF, "MSE")
+smVarBF <- fit.StVariogram(sampleVar, smVar, method="L-BFGS-B", fit.method=7, lower=pars.l, upper=pars.u)
+MSEsmVarBF <- attr(smVarBF, "MSE")
+
+######################
+#simple sum-metric
+######################
+#like sum metric but restrict all components to have a single nugget
+
+ssmVar <- vgmST("simpleSumMetric", 
+                space=vgm(15, "Exp", 60, 0),   #sill, model, range, nugget
+                time=vgm(15, "Exp", 40, 0),
+                joint=vgm(15,"Exp",80,0),
+                nugget=1, stAni=77)
+ssmVarDF <- fit.StVariogram(sampleVar, ssmVar, method="L-BFGS-B", fit.method=0)  
+MSEssmVarDF <- attr(ssmVarDF, "MSE")
+ssmVarBF <- fit.StVariogram(sampleVar, ssmVar, method="L-BFGS-B", fit.method=7, lower=pars.l, upper=pars.u)
+MSEssmVarBF <- attr(ssmVarBF, "MSE")
+
+#####################
+#separable model
+#####################
+
+#not using since bad assumptions, but documentation and scripts below:
+#http://geostat-course.org/system/files/part01.pdf: assumptions of isotropy and stationarity
+#http://www.r-bloggers.com/spatio-temporal-kriging-in-r/: pararmeter selection for vgmST doesn't seem to matter so much 
+# sepVar <- vgmST("separable", 
+#                   space=vgm(15, "Exp", 60, 0),   #sill, model, range, nugget
+#                   time=vgm(15, "Exp", 40, 0),
+#                   sill = 30,
+#                   stAni = 100)
+# 
+# #check how well the model fits data with fit.method=0 which keeps the model but calculates MSE compared to actual data
+# sepVarDF <- fit.StVariogram(sampleVar, sepVar, method="L-BFGS-B", fit.method=0)  #data fit
+# MSEsepVarDF <- attr(sepVarDF, "MSE")  #think error of the eye fit
+# sepVarBF <- fit.StVariogram(sampleVar, sepVar, method="L-BFGS-B", fit.method=7, lower=pars.l, upper=pars.u)
+# MSEsepVarBF <- attr(sepVarBF, "MSE") 
+#extractPar(sepVarBF)
+
+
+##########################################
+#MODEL COMPARISON
+###########################################
+
+#space-time-kriging document
+#sampleVar
+#plot empirical vars
+#create version of table 2, figure 4 shows space-time biases
+#find model with smallest RMSE
+
+#plot all 2D
+plot(sampleVar, list(metVarBF, smVarBF, ssmVarBF), all=T) #psVarBF add once k issue fixed
+
+#plot all 3D
+plot(sampleVar, list(metVarBF, smVarBF, ssmVarBF), all=T, wireframe=T, zlim=c(0,120),  #psVarBF add once k issue fixed
+     zlab=NULL, xlab=list("distance (km)", rot=30),
+     ylab=list("time lag (days)", rot=-35),
+     scales=list(arrows=F, z = list(distance = 5)))
+
+#plot fit metrics (data fit)
+MSE_DF <- c(MSEmetVarDF, MSEsmVarDF, MSEssmVarDF) #add ps
+barplot(MSE_DF, main="MSE of data fit", ylab="MSE", xlab = "Variogram model",
+        names.arg=c("Metric", "Simple-metric", "Simple-sum-metric"))
+
+#plot fit metrics (best fit)
+MSE_BF <- c(MSEmetVarBF, MSEsmVarBF, MSEssmVarBF)  #add ps
+barplot(MSE_BF, main="MSE of best fit", ylab="MSE", xlab = "Variogram model",
+        names.arg=c("Metric", "Simple-metric", "Simple-sum-metric"))
+
+
+#create new spatiotemporal prediction grid
+pred<-krigeST(LOG_ELEV~1, data=sto, newdata=NEWDATAGRID, sepVar, nmax=50)
+stplot(pred)
+
+
+#leave-one-out cross-validation
+#plot figure 6, differences at locations, and 7, differences through time
 
 
 
-#variogram assumptions
-Instead of using a different variogram for each day, we assume a constant model and use one variogram relying on
-all data treating each day as a copy of the same process (pooled variogram) or averaging the variograms (mean
-                                                variogram). The one estimated variogram is then used for
-each day. This way, all information is used but again no
-temporal interactions are incorporated.
 
-
-
-
-
-#1.  Create SpatialPointsDataFrame
-
-coordinates(d) =~ LON+LAT
-projection(d)=CRS('+init=epsg:4326')  #WGS84
-#transform to Mercator to read variogram in meters not degrees
-d <- spTransform(d,CRS('+init=epsg:3395'))  
-
-#2.  Create STIDF object (unstructured space-time objects)
-
-dsp <- SpatialPoints(d@coords,CRS('+init=epsg:3395'))
-
-#check for duplicate points
-dupl <- zerodist(dsp)
-df <- data.frame(d[-dupl[,2]])
-
-
-
-
-
-tm <- as.POSIXct(d$DATE[-dupl[,2]])
-timeDF <- STIDF(dsp,tm,data=df)
-
-var <- variogramST(ELEV~1, data=timeDF, tunit="months", assumeRegular=F, na.omit=T)
-
-
-
-
-
-#182 observations, 2000.01 to 2014.12, monthly
-
-quality <- apply(data, MARGIN=1, FUN = function(x) length(x[!is.na(x)]))
-#drop wells with fewer than 14 obs?
-#n = 14345 wells total
-#n = 5989 with > 14 observations
-
-#lat/lon
+#fit tables:
+MSEmetVarDF <- attr(metVarDF, "MSE")
+MSEmetVarBF <- attr(metVarBF, "MSE") 
