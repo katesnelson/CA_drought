13ec9bfb92a2a9c2b70c41c6d9cb562ad6cddd54
Emily Burchfield <emily@ornette.cas.vanderbilt.edu>
2016-06-30 05:14:58 -0500

    final krig code with memory error

--- Code/gw_kriging.R
+++ Code/gw_kriging.R
@@ -1,3 +1,7 @@
+#questions/issues:
+#should we do any additional pre-processing of the elevation data (standardize, log, etc)?
+#space-time SAMPLE variogram, does not work with neg values
+
 #http://www.r-bloggers.com/spatio-temporal-kriging-in-r/
 #http://localhost/rstudio//files/R/x86_64-pc-linux-gnu-library/3.2/gstat/doc/st.pdf
 
@@ -11,11 +15,7 @@ library(ggplot2)
 library(rgdal)
 library(raster)
 
-#jg direction in space-time variography
-#what's up with variogram bumps
-#any preprocessing of groundwater elevtaion data?  detrend?  log
-
-#home comp
+#ekb comp
 #dr <- 'C:\\Users\\Emily\\Dropbox\\Vanderbilt\\Kate_Emily\\CA_drought\\Code\\'
 #d <- as.data.frame(read.table(paste(dr, 'dsub.txt', sep=''), header=T), stringsAsFactors=F)
 
@@ -23,15 +23,14 @@ library(raster)
 d <- as.data.frame(read.csv(paste('/data/emily/WF/kate/gw/gw.csv', sep=','), header=T), stringsAsFactors=F)
 coordinates(d) <- ~LON+LAT
 
-#create space-time object, http://www.inside-r.org/packages/cran/spacetime/docs/stConstruct
-#take log of elev for more reasonable numbers, but negative and near-zero numbers cause NANs
+#load data
 df <- data.frame(d@data$ELEV, as.Date(d@data$DATE), as.data.frame(d@coords[,1]), as.data.frame(d@coords[,2]))
 names(df) <- c("ELEV", "TIME", "LON", "LAT")
 
 #clean up outliers, drop values less than -50 (10%) and more than 3000 (2%)
 df <- subset(df, df$ELEV >= -50 & df$ELEV < 3000, select=c("ELEV", "TIME", "LON", "LAT"))
 
-#random subset of data
+#select random subset of data (10%)
 dfs <- df[sample(nrow(df), 0.1*nrow(df)), ]
 
 #space-time object
@@ -39,28 +38,24 @@ sto <- stConstruct(dfs, c("LAT", "LON"), "TIME", interval = FALSE)  #time instan
 sto@sp@proj4string <- CRS('+init=epsg:4326')  #WGS84
 stplot(sto)
 
-#space-time SAMPLE variogram, does not work with neg values
-#avg width of CA Central Valley is 64 km, 0.58*64 is 37 --> round up to 40 for spatial range
-#acf is nonstationary, but most change occurs quickly within the first month, so smaller interval should be used, too small and you get holes
-#from acf most significance within 6 months or 180 days 
+#create sample variogram, NOTE: takes awhile to run
 #sampleVar <- variogramST(ELEV~1, sto, tunit="days", tlags=seq(from=0,to=225,by=15), cutoff = 40, na.omit=T)  #log elevation doesn't work because of negative values
 #write.table(sampleVar, 'sampleVar.csv', sep=",")
 sampleVar <- read.table('sampleVar.csv', sep=",")
 
+  #avg width of Central Valley is 64 km, 0.58*64 is 37; round up to 40 for spatial range
+  #acf(sto@data, 'xts')  #up to 200 days significant
+  #acf is nonstationary, but most change occurs quickly within the first month, so smaller interval should be used, too small and you get holes
+  #from acf most significance within 6 months or 180 days 
+
+
 #diagnostic plots
-acf(sto@data, 'xts')  #up to 200 days significant
-plot(sampleVar, map=F) #range of around 60, nugget less than 1
-plot(sampleVar, wireframe=T) #visualize space/time lag
-#for unprojected great circle distances (km).
-#gamma values conditional on treatment of data (log, etc)
+plot(sampleVar, map=F) 
+plot(sampleVar, wireframe=T) 
+#NOTE:  gamma values conditional on treatment of data (log, etc)
 
-#estimate space-time anisotropy, or directional dependence of data
-#http://finzi.psych.upenn.edu/library/gstat/html/estiStAni.html
-estiStAni(sampleVar, c(10,150), "vgm", vgm(20,"Exp",120,0))
-#data too sparse?  http://stats.stackexchange.com/questions/35316/problems-estimating-anisotropy-parameters-for-a-spatial-model
 
-#parameter range
-#paramcheck:http://giv-graeler.uni-muenster.de:3838/spacetime/
+#parameter range, http://giv-graeler.uni-muenster.de:3838/spacetime/; provide huge range here
 pars.l <- c(sill.s = 0.1, range.s = 10, nugget.s = 0.1, sill.t = 0.1, range.t = 10, 
             nugget.t = 0.1, sill.st = 0.1, range.st = 10, nugget.st = 0.1, anis = 0.1)
 pars.u <- c(sill.s = 500000, range.s = 200000, nugget.s = 3000, sill.t = 300000, range.t = 200000, 
@@ -69,6 +64,7 @@ pars.u <- c(sill.s = 500000, range.s = 200000, nugget.s = 3000, sill.t = 300000,
 ##################
 #metric model
 ##################
+
 #http://geostat-course.org/system/files/part01.pdf
 #Temporal domain rescaled to match spatial one.  Lag classes include space, time, and space-time distances.  
 #Using a single model forces all distances to behave in the same way (unlikely)
@@ -159,17 +155,11 @@ MSEssmVarBF <- attr(ssmVarBF, "MSE")
 #MODEL COMPARISON
 ###########################################
 
-#space-time-kriging document
-#sampleVar
-#plot empirical vars
-#create version of table 2, figure 4 shows space-time biases
-#find model with smallest RMSE
-
 #plot all 2D
 plot(sampleVar, list(metVarBF, smVarBF, ssmVarBF, psVarBF), all=T) 
 
 #plot all 3D
-plot(sampleVar, list(metVarBF, smVarBF, ssmVarBF, psVarBF), all=T, wireframe=T, zlim=c(0,120),  
+plot(sampleVar, list(metVarBF, smVarBF, ssmVarBF, psVarBF), all=T, wireframe=T, 
      zlab=NULL, xlab=list("distance (km)", rot=30),
      ylab=list("time lag (days)", rot=-35),
      scales=list(arrows=F, z = list(distance = 5)))
@@ -184,32 +174,30 @@ MSE_BF <- c(MSEmetVarBF, MSEsmVarBF, MSEssmVarBF, MSEpsVarBF)
 barplot(MSE_BF, main="MSE of best fit", ylab="MSE", xlab = "Variogram model",
         names.arg=c("Metric", "Simple-metric", "Sum-metric", "Product-sum"))
 
+#product sum wins
 
 #############################################################
 # Prediction grid construction
 #############################################################
 
-#create spatial reference
-newproj <- '+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs'
-rast <- raster()
-extent(rast) <- extent(d)
-rast <- as(rast, 'SpatialPixels') #360 x 180 cells
-res(rast@grid@cells.dim) <- c(180,90)
+#create empty spatial object
+so <- raster()
+extent(so) <- extent(d)
+#8.9858 degrees x by 7.13729 degrees y, 1 degree, 110 km... approx 2.75 km cell size
+so <- as(so, 'SpatialPoints') #360 x 180 cells
+gridded(so) <- TRUE
+so@proj4string <- sto@sp@proj4string  #set sto proj
 
-
-#create time object
-#tm.grid <- dfs$TIME[order(dfs$TIME)]
+#create monthly time object
 tm.grid <- seq(as.Date("2000/1/1"), by = "month", length.out = 12*15)
 
-tm.grid <- seq(as.Date("2000/1/1"), by = "month", length.out = 3)
 #sto frame
-grid.st <- STF(rast, tm.grid)
-proj4string(grid.st) <- sto@sp@proj4string  #set to same proj as sto
+grid.st <- STF(sp = as(so, "SpatialPoints"), time = tm.grid)
 
 #create new spatiotemporal prediction grid
-pred<-krigeST(ELEV~1, data=sto, newdata=grid.st, modelList=psVarBF)
+pred <- krigeST(ELEV~1, data=sto, newdata=grid.st, modelList=psVarBF, bufferNmax=1, progress=T)
+gridded(pred@sp) <- TRUE 
 stplot(pred)
-
 
 #leave-one-out cross-validation
 #plot figure 6, differences at locations, and 7, differences through time
