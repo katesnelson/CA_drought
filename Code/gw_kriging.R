ecfa31f52118d6fc7ec4ca97b817f8c4ef3d439c
Emily Burchfield <emily@ornette.cas.vanderbilt.edu>
2016-08-18 15:25:45 -0500

    updates to rmse function

--- Code/gw_kriging.R
+++ Code/gw_kriging.R
@@ -51,7 +51,7 @@ sto <- spTransform(sto, CRS('+init=epsg:4326'))  #for the construction of the sa
 #stplot(sto)
 
 #create sample variogram
-sampleVar <- variogramST(ELEV~1, sto, tunit="days", tlags=seq(from=0,to=180,by=15), cutoff = 60, na.omit=T, progress=T)  
+#sampleVar <- variogramST(ELEV~1, sto, tunit="days", tlags=seq(from=0,to=180,by=15), cutoff = 60, na.omit=T, progress=T)  
 #saveRDS(sampleVar, "sampleVar_0816.rds")
 #test <- readRDS("sampleVar_0816.rds")
 
@@ -242,12 +242,10 @@ grid.st <- STF(sp = as(grd, "SpatialPixels"), time = tm.grid)
 #############################
 pred <- krigeST(ELEV~1, data=sto_krig, newdata=grid.st, nmax=20, modelList=smVarBF, stAni=smVarBF$stAni, progress=T) 
 gridded(pred@sp) <- TRUE 
-pred_rmse <- krig_rmse(pred, 1000)
 
 #adjustment to stAni
 pred2 <- krigeST(ELEV~1, data=sto_krig, newdata=grid.st, nmax=20, modelList=smVarBF, stAni=smVarBF$stAni/24/3600, progress=T) 
 gridded(pred2@sp) <- TRUE 
-pred2_rmse <- krig_rmse(pred2, 1000)
 
 #change spatial range, joint range and stAni
 smVarBF_t <- smVarBF
@@ -257,22 +255,21 @@ smVarBF_t$stAni <- smVarBF_t$stAni*1000
 
 pred3 <- krigeST(ELEV~1, data=sto_krig, newdata=grid.st, nmax=20, modelList=smVarBF_t, stAni=smVarBF_t$stAni/24/3600, progress=T) 
 gridded(pred3@sp) <- TRUE 
-pred3_rmse <- krig_rmse(pred3, 1000)
 
-#could try without stAni adjustment
+pred3.5 <- krigeST(ELEV~1, data=sto_krig, newdata=grid.st, nmax=40, modelList=smVarBF_t, stAni=smVarBF_t$stAni/24/3600, progress=T) 
+gridded(pred3@sp) <- TRUE 
 
 #############################
 #simple sum-metric
 #############################
 
-pred4 <- krigeST(ELEV~1, data=sto_krig, newdata=grid.st, nmax=20, modelList=ssmVarBF, stAni=ssmVarBF_t$stAni, progress=T) 
-gridded(pred4@sp) <- TRUE 
-pred4_rmse <- krig_rmse(pred4, 1000)
+# pred4 <- krigeST(ELEV~1, data=sto_krig, newdata=grid.st, nmax=20, modelList=ssmVarBF, stAni=ssmVarBF_t$stAni, progress=T) 
+# gridded(pred4@sp) <- TRUE 
+# 
+# #adjustment to stAni
+# pred5 <- krigeST(ELEV~1, data=sto_krig, newdata=grid.st, nmax=20, modelList=ssmVarBF, stAni=ssmVarBF$stAni/24/3600, progress=T) 
+# gridded(pred5@sp) <- TRUE
 
-#adjustment to stAni
-pred5 <- krigeST(ELEV~1, data=sto_krig, newdata=grid.st, nmax=20, modelList=ssmVarBF, stAni=ssmVarBF$stAni/24/3600, progress=T) 
-gridded(pred5@sp) <- TRUE
-pred5_rmse <- krig_rmse(pred5, 1000)
 
 #change spatial range, joint range and stAni
 ssmVarBF_t <- ssmVarBF  #MAKE COPY
@@ -282,72 +279,181 @@ ssmVarBF_t$stAni <- ssmVarBF_t$stAni*1000
 
 pred6 <- krigeST(ELEV~1, data=sto_krig, newdata=grid.st, nmax=20, modelList=ssmVarBF_t, stAni=ssmVarBF_t$stAni/24/3600, progress=T) 
 gridded(pred6@sp) <- TRUE 
-pred6_rmse <- krig_rmse(pred6, 1000)
 
+pred6.5 <- krigeST(ELEV~1, data=sto_krig, newdata=grid.st, nmax=40, modelList=ssmVarBF_t, stAni=ssmVarBF_t$stAni/24/3600, progress=T) 
+gridded(pred6@sp) <- TRUE 
+
+
+###################################################
+#compute res/rmse
+###################################################
+#build residual datasets
+n <- 1000
+pred_rmse <- krig_rmse(pred, n)
+pred2_rmse <- krig_rmse(pred2, n)
+pred3_rmse <- krig_rmse(pred3, n)
+pred3.5_rmse <- krig_rmse(pred3.5, n)
+pred4_rmse <- krig_rmse(pred4, n)
+pred5_rmse <- krig_rmse(pred5, n)
+pred6_rmse <- krig_rmse(pred6, n)
+pred6.5_rmse <- krig_rmse(pred6.5, n)
+
+
+#RMSE
+rmse0 <- rmse(pred_rmse$elev_obs, pred_rmse$elev_pred) 
+rmse2 <- rmse(pred2_rmse$elev_obs, pred2_rmse$elev_pred) 
+rmse3 <- rmse(pred3_rmse$elev_obs, pred3_rmse$elev_pred) 
+rmse3.5 <- rmse(pred3.5_rmse$elev_obs, pred3.5_rmse$elev_pred) 
+rmse4 <- rmse(pred4_rmse$elev_obs, pred4_rmse$elev_pred) 
+rmse5 <- rmse(pred5_rmse$elev_obs, pred5_rmse$elev_pred) 
+rmse6 <- rmse(pred6_rmse$elev_obs, pred6_rmse$elev_pred) 
+rmse6.5 <- rmse(pred6.5_rmse$elev_obs, pred6.5_rmse$elev_pred) 
+
+#MSE
+mse0 <- mse(pred_rmse$elev_obs, pred_rmse$elev_pred) 
+mse2 <- mse(pred2_rmse$elev_obs, pred2_rmse$elev_pred) 
+mse3 <- mse(pred3_rmse$elev_obs, pred3_rmse$elev_pred) 
+mse3.5 <- mse(pred3.5_rmse$elev_obs, pred3.5_rmse$elev_pred) 
+mse4 <- mse(pred4_rmse$elev_obs, pred4_rmse$elev_pred) 
+mse5 <-mse(pred5_rmse$elev_obs, pred5_rmse$elev_pred) 
+mse6 <- mse(pred6_rmse$elev_obs, pred6_rmse$elev_pred) 
+mse6.5 <- mse(pred6.5_rmse$elev_obs, pred6.5_rmse$elev_pred) 
+
+#pred3 best with both MSE adn RMSE
 
 ############################################################################################################
 #CROSS VALIDATION
 ############################################################################################################
 
 #random sample of held-out data (OBSERVED)
-held_out_sample <- held_out[sample(nrow(held_out), 0.01*nrow(held_out)), ]  #take full dataset now
+held_out_sample <- held_out #[sample(nrow(held_out), 0.01*nrow(held_out)), ]  #take full dataset now
 held_out_sto <- stConstruct(held_out_sample, c("LAT", "LON"), "TIME", interval = FALSE)  
 held_out_sto@sp@proj4string <- CRS('+init=epsg:3310')
 held_out_sp <- as(held_out_sto, "Spatial")
-time_list_obs <- unique(held_out_sp@data$time)
+time_list_obs <- unique(held_out_sp@data$time)  #full list of observations in time
 
 krig_rmse <- function(krigged_data, validation_sample_size) {
-  
   residuals <- list()
-  predsp <- as(krigged_data, "Spatial") #spatial pixels dataframe
+  predsp <- as(krigged_data, "Spatial") 
   gridded(predsp) <- TRUE
   time_list_pred <- unique(attr(predsp, "time"))
   
   for (i in 1:validation_sample_size) {
     
-    #predicted data
-    random_time <- sample(1:length(time_list_pred), 1)
-    time_pred <- time_list_pred[random_time]
-    names(time_pred) <- "time_pred"
-    space_pred <- raster(predsp[random_time])
-  
-    #observed data
-    time_gt <- time_list_obs[time_list_obs > time_pred]
-    if (length(time_gt) == 0) {
-      time_lt <- time_list_obs[time_list_obs < time_pred]
-      time_obs <- tail(time_lt, n=1)
-    } else {
-      time_obs <- time_gt[1]
-    }
-    space_obs <- held_out_sp[held_out_sp$time == time_obs,]
-    elev_obs <- space_obs$ELEV[1]
-    coor <- space_obs@coords[1,]
-    lon <- coor[1]
+    #OBSERVED DATASET
+    #select point in time
+    random_time <- sample(1:length(time_list_obs), 1)  
+    time_obs <- time_list_obs[random_time]   
+    names(time_obs) <- "time_obs"
+    #one observed point in space
+    space_obs <- held_out_sp[held_out_sp$time == time_obs,]  
+    random_space_obs <- sample(1:length(space_obs$ELEV), 1)
+    space_obs <- space_obs[random_space_obs,]
+    elev_obs <- space_obs$ELEV
+    coor_obs <- space_obs@coords[random_space_obs,]
+    lon <- coor_obs[1]
     names(lon) <- "lon"
-    lat <- coor[2]
+    lat <- coor_obs[2]
     names(lat) <- "lat"
     names(elev_obs) <- "elev_obs"
+    #PREDICTED DATASET
+    #time
+    time_pred = paste(substr(as.character(time_obs), 1,7),"-01", sep="")
+    #space
+    space_pred <- raster(predsp[time_pred])
     
-    #extract predicted data from observed data point
-    elev_pred <- extract(space_pred, space_obs)[1]
+    #EXTRACT PREDICTED RASTER DATA FROM OBSERVED POINT 
+    elev_pred <- extract(space_pred, space_obs@coords)
     names(elev_pred) <- "elev_pred"
     
     #final data entry
-    residuals[[i]] <- list(elev_obs, elev_pred, time_pred, lon, lat)
+    residuals[[i]] <- list(lon, lat, time_obs, elev_obs, time_pred, elev_pred)  #add time_obs, add grid.index coords for predicted
   }
   
   res <- as.data.frame(do.call(rbind, lapply(residuals, unlist)))
   res <- na.omit(res)
   res$time_pred <- as.Date(res$time_pred)
-  
-  #mse <- mse(res$elev_obs, res$elev_pred)
-  rmse_final <- rmse(res$elev_obs, res$elev_pred)
-  return(rmse_final)
-  
+  res$time_obs <- as.Date(res$time_obs)
+
+  return(res)  #return list, MSE, RMSE and residuals
 }
+
+#build near full res dataset
+res <- krig_rmse(pred2, 47000)
+res$elev_delta <- res$elev_obs - res$elev_pred
+hist(res$elev_delta, 1000)  #what's up with -4000 and 4000 observations?
+
+#sto_res <- stConstruct(res, c("lat", "lon"), "time_pred", interval = FALSE)  #time instance, not time interval
+#stplot(sto_res)
+
+#plot RMSE value at a point in space (held out) through all time obs
+time_obs <- res%>% group_by(time_pred) %>% summarize(mean(elev_obs))
+time_pred <- res%>% group_by(time_pred) %>% summarize(mean(elev_pred))
+time_m <- res %>% group_by(time_pred) %>% summarize(mean(elev_delta))
+time_sd <- res %>% group_by(time_pred) %>% summarize(sd(elev_delta))
+time_count <- held_out_sample%>% group_by(TIME) %>% summarize(count=n())
+
+plot(time_m, type="l", main="obs-pred", col="black", ylim=c(-10, 10))
+lines(time_obs, type='l', col='blue', lty=2)
+lines(time_pred, type='l', col='red', lty=2)
+#lines(time_count, type='l', col='green')
+legend("topright", c("obs", "pred"), lty=c(1,1), lwd=c(2.5,2.5), col=c("blue","red")) 
+ 
+#space
+space_m <- data.frame(res %>% group_by(lat,lon) %>% summarize(mean(elev_delta)))
+space_sd <- data.frame(res %>% group_by(lat,lon) %>% summarize(sd(elev_delta)))
+coordinates(space_m) <- ~lon+lat
+space_m@proj4string <- CRS('+init=epsg:3310') #confirm original projection
+coordinates(space_sd) <- ~lon+lat
+space_sd@proj4string <- CRS('+init=epsg:3310') #confirm original projection
+spplot(space_m, main="obs-pred mean") #CHANGE SYMBOLOGY
+spplot(space_sd, main="obs-pred sd")
+
+space_m_obs <- data.frame(res %>% group_by(lat,lon) %>% summarize(max(elev_obs)))
+coordinates(space_m_obs) <- ~lon+lat
+space_m_obs@proj4string <- CRS('+init=epsg:3310') #confirm original projection
+spplot(space_m_obs, main="observed max")
+
+space_m_p <- data.frame(res %>% group_by(lat,lon) %>% summarize(max(elev_pred)))
+coordinates(space_m_p) <- ~lon+lat
+space_m_p@proj4string <- CRS('+init=epsg:3310') #confirm original projection
+spplot(space_m_p, main="predicted max")
+#plot number of observations in time in grid cell
+
+#wonky 4000s are not in observed data:
+space_m_p <- data.frame(dfs_clean %>% group_by(LAT,LON) %>% summarize(max(ELEV)))
+coordinates(space_m_p) <- ~LON+LAT
+space_m_p@proj4string <- CRS('+init=epsg:3310') #confirm original projection
+spplot(space_m_p, main="dfs_clean max")
+
+
+
+#plot space-time fit
+#extract all spatial observations at one point in time
+select_time <- pred2@data$var1.pred[pred3@time$timeIndex == 6]
+
+
+#plot point through time
+#predicted
+grid_id <- unique(pred2@sp@grid.index)
+obs <- sample(grid_id, 1)
+coords <- pred2@sp@coords[pred3@sp@grid.index == obs]
+select_space <- pred2@data$var1.pred[pred2@sp@grid.index == obs]
+plot(select_space, main=paste(obs, coords))
+
+#observed
+obs <- sample(unique(sto@sp@coords), 1)
+coords <- sto@sp@coords[obs,]  
+select_space <- sto@data$ELEV[sto@sp@coords == coords]
+plot(select_space, main=paste(obs, coords))
+
+
+
 
 
 
 #back transform
-constant_transform <- abs(min(df$ELEV)) + 1 #linear transformation to remove negative values
-df$ELEV <- log(df$ELEV + constant_transform) #normally distributed
+#res$elev_obs_t <- exp(res$elev_obs) - constant_transform
+#res$elev_pred_t <- exp(res$elev_pred) - constant_transform
+#difference between observed and predicted                                  
+
